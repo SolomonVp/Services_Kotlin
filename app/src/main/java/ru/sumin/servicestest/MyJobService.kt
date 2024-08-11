0303a384e2ea9b4830d6b70c8a6350c633586a16
SolomonVP <Lum7023@yandex.ru>
2024-08-12 00:09:20 +0300

    JobScheduler. Метод enqueue

--- app/src/main/java/ru/sumin/servicestest/MyJobService.kt
+++ app/src/main/java/ru/sumin/servicestest/MyJobService.kt
@@ -2,6 +2,9 @@ package ru.sumin.servicestest
 
 import android.app.job.JobParameters
 import android.app.job.JobService
+import android.content.Intent
+import android.os.Build
+import android.os.PersistableBundle
 import android.util.Log
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
@@ -9,7 +12,7 @@ import kotlinx.coroutines.cancel
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 
-class MyJobService: JobService() {
+class MyJobService : JobService() {
 
     private val coroutineScope = CoroutineScope(Dispatchers.Main)
 
@@ -20,12 +23,22 @@ class MyJobService: JobService() {
 
     override fun onStartJob(params: JobParameters?): Boolean {
         log("onStartCommand")
-        coroutineScope.launch {
-            for (i in 0 until 100) {
-                delay(1000)
-                log("Timer $i")
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            coroutineScope.launch {
+                var workItem = params?.dequeueWork()
+                while (workItem != null) {
+                    val page = workItem.intent.getIntExtra(PAGE, 0)
+
+                    for (i in 0 until 5) {
+                        delay(1000)
+                        log("Timer $i $page")
+                    }
+                    params?.completeWork(workItem)
+                    workItem = params?.dequeueWork()
+                }
+                jobFinished(params, false)
             }
-            jobFinished(params, true)
         }
         return true
     }
@@ -48,5 +61,13 @@ class MyJobService: JobService() {
     companion object {
 
         const val JOB_ID = 111
+
+        private const val PAGE = "page"
+
+        fun newIntent(page: Int): Intent {
+            return Intent().apply {
+                putExtra(PAGE, page)
+            }
+        }
     }
 }
